3b1491d6238f6e07ed96e50b4a53c7cdb0db9dcb
Jody Lock <jodylock@jodylock.com>
2016-03-28 09:25:46 +1000

    Update index.php

--- index.php
+++ index.php
@@ -25,13 +25,13 @@
 	function displayShow($colour){
 
 		// using cURL to make the http request to access data from the outside web page
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "http://api.tvmaze.com/search/shows?q=$colour");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, FALSE);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
-		$response = curl_exec($ch);
-		curl_close($ch);
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, "http://api.tvmaze.com/search/shows?q=$colour");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_HEADER, FALSE);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+		$response = curl_exec($curl);
+		curl_close($curl);
 		$result = json_decode($response, true);
 
 			// for loop to list the results
